b7a2b2baf4d21dbea155b990dd5aee7f6eb7f6c6
nilatac22 <nilatac22@gmail.com>
2021-05-10 16:27:33 +0300

    Update server.js

--- day7/server.js
+++ day7/server.js
@@ -7,7 +7,7 @@ const app = express()
 const port = 3000
 app.get('/', (req, res) =>{
 var con = mysql.createConnection({
- host: "localhost",
+ host: "mysql",
  user: "catalin",
  password: "catalin",
  database: "day7"
